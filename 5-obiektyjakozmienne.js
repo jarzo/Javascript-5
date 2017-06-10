61a46ca6d2856312b6fac068dcbb6a8b49af1575
Michał Jarząbek <nebraska994@gmail.com>
2017-06-10 11:35:26 +0200

    Obiekty

--- 5-obiektyjakozmienne.js
+++ 5-obiektyjakozmienne.js
@@ -1,1 +1,20 @@
 'use strict'
+
+
+/*
+
+Obiekty jako zmienne
+
+*/
+
+var student = {
+	imie: "Kaja",
+	wzrost: 152,
+	wyswietlDane: function() { console.log(this.imie)}
+}
+
+student.wyswietlDane();
+
+console.log(student);
+
+//sa 2 typy referencyjne czyli złożone: tablicowe i obiketowe, do prostych należą stringi, liczny, logiczne//
